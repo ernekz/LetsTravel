b08d808446addc80e312e50582729a5ec891e4d2
Ernekz <specialbmx@gmail.com>
2023-08-24 14:43:14 +0200

    Initial Commit

---
+++ LetsTravel/LetsTravelApp.swift
@@ -0,0 +1,20 @@
+//
+//  LetsTravelApp.swift
+//  LetsTravel
+//
+//  Created by Ernestas Kazinevicius on 2023-08-24.
+//
+
+import SwiftUI
+
+@main
+struct LetsTravelApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
